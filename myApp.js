3f745e4ccc88ba67cd10a032f7c38e881d8847bf
Glitch (spark-cyan-atom) <none>
2020-05-16 04:21:06 +0000

    🌛👨‍❤️‍👨 Checkpoint ./myApp.js:15670943/436

--- myApp.js
+++ myApp.js
@@ -1,4 +1,3 @@
-console.log("Hello World")
 var express = require('express');
 var app = express();
 
@@ -9,12 +8,20 @@ var app = express();
 
 
 /** 1) Meet the node console. */
+//console.log("Hello World")
 
 
 /** 2) A first working Express Server */
+/* app.get('/', (req, res) => {
+  res.send('Hello Express')
+}) */
 
 
 /** 3) Serve an HTML file */
+app.get('/', (req, res) => {  
+  absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);
+})
 
 
 /** 4) Serve static assets  */
